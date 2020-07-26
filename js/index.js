23c4e5420545343575fa983a38e09cd92f664681
Sonjoy Barmon <60999976+sonjoybarmon@users.noreply.github.com>
2020-07-26 23:08:31 +0600

    tax set

--- js/index.js
+++ js/index.js
@@ -1,127 +1,3 @@
-//minus1 button 
-const minusButton1 = document.getElementById("minusBtn1");
-    minusButton1.addEventListener("click", function(){
-const minusInputValue = document.getElementById("inputValue1").value;
-const minusInputNumber = parseFloat(minusInputValue);
-    if (minusInputNumber == 0){
-        let newValue = 00;
-        document.getElementById('inputValue1').value = newValue;
-    } else {
-        let newValue = minusInputNumber - 1;
-        document.getElementById('inputValue1').value = newValue;
-    } 
-
-const removePrince1 = document.getElementById("currentPrice2").innerText;
-const removeNumber1 =parseFloat(removePrince1);
-const removeInput1 = document.getElementById("inputValue1").value;
-    let newRemoveInput = removeInput1 * 1219;
-    document.getElementById("currentPrice1").innerText = newRemoveInput;
-
-const addSubtotal = document.getElementById("subtotal").innerText;
-const addSubtotalNumber = parseFloat(addSubtotal);
-const subtotalInput1 = document.getElementById("currentPrice1").innerText;
-const subtotalInput1Num = parseFloat(subtotalInput1);
-const subtotalInput2 = document.getElementById("currentPrice2").innerText;
-const inputValue2Num = parseFloat(subtotalInput2);
-    let newSubTotal = subtotalInput1Num + inputValue2Num ;
-    document.getElementById("subtotal").innerText = newSubTotal;
-    document.getElementById("total").innerText = newSubTotal;
-
-});
-
-
-//minus2 button
-const minusButton2 = document.getElementById("minusBtn2").addEventListener("click", function(){
-    minusInputValue2 = document.getElementById("inputValue2").value;
-    const minusInputValue2Num = parseFloat(minusInputValue2);
-    if (minusInputValue2Num == 0) {
-        let newValue = 00;
-        document.getElementById("inputValue2").value = newValue;
-    } else {
-        let newValue = minusInputValue2Num - 1 ;
-        document.getElementById("inputValue2").value = newValue;
-    }
-
-const removePrince2 = document.getElementById("currentPrice2").innerText;
-const removeNumber2 = parseFloat(removePrince2);
-const removeValue = document.getElementById("inputValue2").value;
-    let newValue = removeValue * 59;
-    document.getElementById("currentPrice2").innerText = newValue;
-
-
-const addSubtotal = document.getElementById("subtotal").innerText;
-const addSubtotalNumber = parseFloat(addSubtotal);
-const subtotalInput1 = document.getElementById("currentPrice1").innerText;
-const subtotalInput1Num = parseFloat(subtotalInput1);
-const subtotalInput2 = document.getElementById("currentPrice2").innerText;
-const inputValue2Num = parseFloat(subtotalInput2);
-    let newSubTotal = subtotalInput1Num + inputValue2Num ;
-    document.getElementById("subtotal").innerText = newSubTotal;
-    document.getElementById("total").innerText = newSubTotal;
-});
-
-
-
-//plus1 button
-const plusButton = document.getElementById("plusBtn1");
-    plusButton.addEventListener("click", function(){
-const plusInputValue = document.getElementById("inputValue1").value;
-const plusInputNumber = parseFloat(plusInputValue);
-    let newValue = plusInputNumber + 1;
-    document.getElementById("inputValue1").value = newValue;
-
-const currentPrice = document.getElementById("currentPrice1").innerText;
-const currentPriceNumber = parseFloat(currentPrice);
-const currentValue = document.getElementById("inputValue1").value;
-    let newPrice = currentValue * 1219 ;
-    document.getElementById("currentPrice1").innerText = newPrice ;
-
-
-
-
-
-const addSubtotal = document.getElementById("subtotal").innerText;
-const addSubtotalNumber = parseFloat(addSubtotal);
-const subtotalInput1 = document.getElementById("currentPrice1").innerText;
-const subtotalInput1Num = parseFloat(subtotalInput1);
-const subtotalInput2 = document.getElementById("currentPrice2").innerText;
-const inputValue2Num = parseFloat(subtotalInput2);
-    let newSubTotal = subtotalInput1Num + inputValue2Num ;
-    document.getElementById("subtotal").innerText = newSubTotal;
-    document.getElementById("total").innerText = newSubTotal;
-
-});
-
-
-
-//plus2 button
-
-const plusButton2 = document.getElementById("plusBtn2").addEventListener("click", function(){
-    const plusInputValue2 = document.getElementById("inputValue2").value;
-    const plusInputNumber2 = parseFloat(plusInputValue2);
-    let newValue = plusInputNumber2 + 1;
-    document.getElementById("inputValue2").value = newValue;
-
-const currentPrice2 =document.getElementById("currentPrice2").innerText;
-const currentPrice2Number = parseFloat(currentPrice2);
-const currentPrice2Value = document.getElementById("inputValue2").value;
-    let newPrice = currentPrice2Value * 59;
-    document.getElementById("currentPrice2").innerText = newPrice; 
-
-
-
-const addSubtotal = document.getElementById("subtotal").innerText;
-const addSubtotalNumber = parseFloat(addSubtotal);
-const subtotalInput1 = document.getElementById("currentPrice1").innerText;
-const subtotalInput1Num = parseFloat(subtotalInput1);
-const subtotalInput2 = document.getElementById("currentPrice2").innerText;
-const inputValue2Num = parseFloat(subtotalInput2);
-    let newSubTotal = subtotalInput1Num + inputValue2Num ;
-    document.getElementById("subtotal").innerText = newSubTotal;
-    document.getElementById("total").innerText = newSubTotal;
-
-});
-
 const nextPage = document.getElementById("next_page").addEventListener("click",function(){
     const nonePage = document.querySelector(".cart");
         nonePage.style.display = "none";
@@ -131,3 +7,105 @@ const nextPage = document.getElementById("next_page").addEventListener("click",f
     const bodyColor = document.querySelector("body");
         bodyColor.style.background="linear-gradient(90deg ,#87cfebc0, #ffffff)";
 });
+//phone-increase handler 
+document.getElementById('phone-increase').addEventListener('click', function(){
+    // handlePhoneChange(true);
+    handleProductChange('phone', true);
+});
+// phone-decrease handler function
+document.getElementById('phone-decrease').addEventListener('click',function(){
+    // handlePhoneChange(false);
+    handleProductChange('phone', false);
+});
+//case-increase handler 
+document.getElementById('case-increase').addEventListener('click', function(){
+    // const caseInput = document.getElementById('case-count').value;
+    // const caseCount = parseInt(caseInput);
+    // const caseNewCount = caseCount + 1;
+    // document.getElementById('case-count').value = caseNewCount;
+    // const caseTotal = caseNewCount * 59 ;
+    // document.getElementById('case-total').innerText = caseTotal ;
+    handleProductChange('case',true);
+});
+// case-decrease handler function
+document.getElementById('case-decrease').addEventListener('click',function(){
+    // const caseInput = document.getElementById('case-count').value;
+    // const caseCount = parseInt(caseInput);
+    // const caseNewCount = caseCount - 1;
+    // document.getElementById('case-count').value = caseNewCount ;
+    // const caseTotal = caseNewCount * 59 ;
+    // document.getElementById('case-total').innerText = caseTotal ;
+    handleProductChange('case', false);
+});
+// //phone inIncrease function
+// function handlePhoneChange(inIncrease){
+//     const phoneInput = document.getElementById('phone-count').value;
+//     const phoneCount = parseInt(phoneInput);
+//     let phoneNewCount = phoneCount;
+//     if (inIncrease == true) {
+//         phoneNewCount = phoneCount + 1;
+//     } if(inIncrease == false && phoneCount > 0 ) {
+//         phoneNewCount = phoneCount - 1;  
+//     }
+//     document.getElementById('phone-count').value = phoneNewCount ;
+//     const caseTotal = phoneNewCount * 1219 ;
+//     document.getElementById('phone-total').innerText = caseTotal ;
+// }
+// //case inIncrease function
+// function handleProductChange(inIncrease){
+//     const caseInput = document.getElementById('case-count').value;
+//     const caseCount = parseInt(caseInput);
+//     let caseNewCount = caseCount;
+//     if (inIncrease == true) {
+//         caseNewCount = caseCount + 1;
+//     } if(inIncrease == false && caseCount > 0 ) {
+//         caseNewCount = caseCount - 1;  
+//     }
+//     document.getElementById('case-count').value = caseNewCount ;
+//     const caseTotal = caseNewCount * 59 ;
+//     document.getElementById('case-total').innerText = caseTotal ;
+// }
+//case inIncrease function
+function handleProductChange(product ,inIncrease){
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    let productNewCount = productCount;
+    if (inIncrease == true) {
+        productNewCount = productCount + 1;
+    } if(inIncrease == false && productCount > 0 ) {
+        productNewCount = productCount - 1;  
+    }
+    productInput.value = productNewCount ;
+    // const productTotal = productNewCount * 59 ;
+    let productTotal = 0 ;
+    if(product == 'phone'){
+        productTotal = productNewCount * 1219 ;
+    }
+    if(product == 'case'){
+        productTotal = productNewCount * 59 ;
+    }
+    document.getElementById(product + '-total').innerText = productTotal ;
+    calculatorTotal() // calculator function
+};
+//calculator all set function
+function calculatorTotal(){
+    // const phoneInput = document.getElementById('phone-count');
+    // const phoneCount = parseInt(phoneInput.value);
+    const phoneCount = getInputValue('phone');
+    // const caseInput = document.getElementById('case-count');
+    // const caseCount = parseInt(caseInput.value);
+    const caseCount = getInputValue('case');
+    const totalCount = phoneCount * 1219 + caseCount * 59 ;
+    const taxAmount =Math.round(totalCount * 0.1);
+    document.getElementById('tax').innerText = taxAmount;
+    const total = totalCount + taxAmount;
+
+    document.getElementById('Sub-total').innerText = totalCount ;
+    document.getElementById('total').innerText = total ;
+};
+//getInputValue function
+function getInputValue(product){
+    const productInput = document.getElementById(product + '-count');
+    const productCount = parseInt(productInput.value);
+    return productCount;
+}
